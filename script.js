c3f5ffec8be684c024321b5e111bdea707ae9599
Renato Dias <natovirtual@gmail.com>
2024-03-10 14:57:45 -0300

    adicionada funcionalidades JS e acessibilidade

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ const searchMovieButton = document.getElementById('search-movie');
 const movieContainer = document.getElementById('movie-container');
 const movieName = document.getElementById('movie-name');
 const movieYear = document.getElementById('year-movie');
+const email = 'natovirtual@gmail.com'
 
 async function insertieMovieData(){   
   try{
-    let url = `https://www.omdbapi.com/?apikey=${key}&t=${generateMovieParameter()}${generateYearParameter()}`;  
+    let url = `https://www.omdbapi.com/?apikey=${key}&t=${await generateMovieParameter()}${generateYearParameter()}`;  
     const response =  await fetch(url);
     const data = await response.json();
-    if(data.Error) throw new Error('Filme não encontrado')    
+    if(data.Error) throw new Error('Filme não encontrado');
+
+    let urlTranslate = `https://api.mymemory.translated.net/get?q=${data.Plot}!&langpair=en|pt-br&de=${email}`;
+    const responseTranslate = await fetch(urlTranslate);
+    const dataTranslate = await responseTranslate.json();
+    const plotTranslate = dataTranslate.responseData.translatedText;
+
+    let urlTranslateGenre = `https://api.mymemory.translated.net/get?q=${data.Genre}!&langpair=en|pt-br&de=${email}`;
+    const responseTranslateGenre = await fetch(urlTranslateGenre);
+    const dataTranslateGenre = await responseTranslateGenre.json();
+    const genreTranslate = dataTranslateGenre.responseData.translatedText;
+
+    movieContainer.classList.add('unhidden-movie-container')
   
     movieContainer.innerHTML = `
       <div id="movie-info" class="d-flex container">
@@ -16,12 +29,13 @@ async function insertieMovieData(){
           <img src="${data.Poster}" alt="${data.Title}">
         </div>
         <div id="movie-data" class="d-flex flex-column">
-           <p>Sinopse: ${data.Plot}</p>
-           <p>Elenco: ${data.Actors}</p>
-            <p>Gênero: ${data.Genre}</p>
+           <p><span>Sinopse:</span> ${plotTranslate}</p>
+           <p><span>Elenco:</span> ${data.Actors}</p>
+           <p><span>Gênero:</span> ${genreTranslate}</p>
+           <p><span>Ano:</span> ${data.Year}</p>
         </div>
       </div>
-          <button class="mt-5">Adicionar aos favoritos <i class="bi bi-star"></i></button>
+          <button class="mt-5" id="add-favorites" onclick="hiddenMovieInfo()">Adicionar aos favoritos <i class="bi bi-star"></i></button>
   `;
   }catch (error){
     notie.alert({type: 'error', text: error.message})
@@ -30,10 +44,15 @@ async function insertieMovieData(){
   
 }
 
-function generateMovieParameter (){
+async function generateMovieParameter (){
   if(movieName.value === '') throw new Error('Nome do filme deve ser informado');   
    
-  const searchMovie = movieName.value.split(' ').join('+')
+  let urlTranslate = `https://api.mymemory.translated.net/get?q=${movieName.value}!&langpair=pt-br|en&de=${email}`;
+  const responseTranslate = await fetch(urlTranslate);
+  const dataTranslate = await responseTranslate.json();
+  const nameMovieTranslate = dataTranslate.responseData.translatedText;   
+   
+  const searchMovie = nameMovieTranslate.split(' ').join('+')
   return searchMovie;
 }
 
